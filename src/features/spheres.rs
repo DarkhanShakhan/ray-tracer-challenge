a2f44dcb908d98a1c9a14a60531e932b726f6111
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2023-09-02 22:52:02 +0600

    fix merge

--- src/features/spheres.rs
+++ src/features/spheres.rs
@@ -35,25 +35,10 @@ impl Sphere {
     fn set_material(&mut self, material: Material) {
         self.material = material;
     }
-    fn set_transform(&mut self, transformation: Matrice) {
+    pub fn set_transform(&mut self, transformation: Matrice) {
         self.transform = transformation;
     }
-    fn intersect(&self, r: &Ray) -> Vec<Intersection> {
-        let r2 = transform(r.clone(), self.transform.inverse().unwrap());
-        let sphere_to_ray = r2.origin - Tuple::point(0.0, 0.0, 0.0);
-        let a = r2.direction.dot(&r2.direction);
-        let b = 2.0 * r2.direction.dot(&sphere_to_ray);
-        let c = sphere_to_ray.dot(&sphere_to_ray) - 1.0;
-        let disrciminant = b * b - 4.0 * a * c;
-        if disrciminant < 0.0 {
-            return vec![];
-        }
-        intersections(&mut [
-            Intersection::new((-b - disrciminant.sqrt()) / (2.0 * a), self.clone()),
-            Intersection::new((-b + disrciminant.sqrt()) / (2.0 * a), self.clone()),
-        ])
-    }
-    fn normal_at(&self, world_point: Tuple) -> Tuple {
+    pub fn normal_at(&self, world_point: Tuple) -> Tuple {
         let object_point = self.transform.inverse().unwrap() * world_point;
         let object_normal = object_point - Tuple::point(0.0, 0.0, 0.0);
         let mut world_normal = self.transform.inverse().unwrap().transpose() * object_normal;
